eb0ca0aa8b962733026ee33f5329ea8980fde7fe
EfrantEmmanuelG <143708104+EfrantEmmanuelG@users.noreply.github.com>
2025-05-18 14:55:11 +0700

    1

--- app.py
+++ app.py
@@ -28,21 +28,21 @@ st.sidebar.header("Formulir Input Pasien")
 # -------------------------------
 # 4. Input user dari sidebar
 # -------------------------------
-pregnancies = st.sidebar.number_input("Jumlah Kehamilan", min_value=0, max_value=20, value=1)
-glucose = st.sidebar.number_input("Glukosa", min_value=0.0, max_value=300.0, value=120.0)
-blood_pressure = st.sidebar.number_input("Tekanan Darah", min_value=0.0, max_value=200.0, value=70.0)
-skin_thickness = st.sidebar.number_input("Ketebalan Kulit", min_value=0.0, max_value=100.0, value=20.0)
-insulin = st.sidebar.number_input("Insulin", min_value=0.0, max_value=1000.0, value=79.0)
-bmi = st.sidebar.number_input("BMI", min_value=0.0, max_value=70.0, value=32.0)
-dpf = st.sidebar.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=3.0, value=0.5)
-age = st.sidebar.number_input("Usia", min_value=1, max_value=120, value=33)
+pregnancies = st.sidebar.number_input("Jumlah Kehamilan", min_value=0, max_value=20, value=0)
+glucose = st.sidebar.number_input("Glukosa", min_value=0.0, max_value=300.0, value=0.0)
+blood_pressure = st.sidebar.number_input("Tekanan Darah", min_value=0.0, max_value=200.0, value=0.0)
+skin_thickness = st.sidebar.number_input("Ketebalan Kulit", min_value=0.0, max_value=100.0, value=0.0)
+insulin = st.sidebar.number_input("Insulin", min_value=0.0, max_value=1000.0, value=0.0)
+bmi = st.sidebar.number_input("BMI", min_value=0.0, max_value=70.0, value=0.0)
+dpf = st.sidebar.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=3.0, value=0.0)
+age = st.sidebar.number_input("Usia", min_value=1, max_value=120, value=1)
 
 input_data = [pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]
 
 # -------------------------------
 # 5. Prediksi & tampilan hasil
 # -------------------------------
-if st.sidebar.button("Diagnosa Sekarang"):
+if st.sidebar.button("Cek Disini"):
 
     # Reshape input
     data_np = np.asarray(input_data).reshape(1, -1)
@@ -50,7 +50,7 @@ if st.sidebar.button("Diagnosa Sekarang"):
     # Prediksi
     prediksi = model.predict(data_np)
 
-    st.subheader("📄 Data yang Dimasukkan")
+    st.subheader("Data yang Dimasukkan")
     df_input = pd.DataFrame([input_data], columns=[
         'Pregnancies','Glucose','BloodPressure','SkinThickness',
         'Insulin','BMI','DiabetesPedigreeFunction','Age'
@@ -58,16 +58,16 @@ if st.sidebar.button("Diagnosa Sekarang"):
     st.dataframe(df_input)
 
     # Hasil Diagnosa
-    st.subheader("🔍 Hasil Diagnosa")
+    st.subheader("Hasil Pengecekan")
     if prediksi[0] == 1:
-        st.error("⚠️ Anda TERDETEKSI Diabetes")
+        st.error("Anda TERDETEKSI Diabetes")
     else:
-        st.success("✅ Anda TIDAK TERDETEKSI Diabetes")
+        st.success("Anda TIDAK TERDETEKSI Diabetes")
 
     # -------------------------------
     # 6. Grafik Perbandingan
     # -------------------------------
-    st.subheader("📊 Perbandingan dengan Rata-rata Dataset")
+    st.subheader("Perbandingan dengan Rata-rata Dataset dengan Input")
     df_diabetes = pd.read_csv("diabetes.csv")
 
     avg_diabetes = df_diabetes[df_diabetes["Outcome"]==1].drop(columns="Outcome").mean()
